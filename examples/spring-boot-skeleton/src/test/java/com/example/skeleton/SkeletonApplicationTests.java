f37b4a5c020ee95af6c60f58a735a94dc7eedb36
Stephen Oakey <83302029+sdoakey@users.noreply.github.com>
2026-02-05 12:04:20 -0500

    Added example Spring Boot skeleton

---
+++ examples/spring-boot-skeleton/src/test/java/com/example/skeleton/SkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test ensures the Spring application context loads successfully
+    }
+}
